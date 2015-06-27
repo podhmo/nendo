244232feadfc8fba11ca430ce836ced66e93fba9
podhmo <podhmo+altair@beproud.jp>
2015-06-27 23:37:31 +0900

    08

---
+++ examples/08.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+
+"""
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE account_id = (SELECT MAX(account_id)
+                    FROM account);
+"""
+
+from nendo import Query, make_record, render, subquery
+from nendo.value import fn
+
+Account = make_record("account", "account_id product_cd open_date avail_balance")
+subq = (Query().from_(Account).select(fn.count(Account.account_id)))
+query = (Query()
+         .from_(Account)
+         .where(Account.account_id == subquery(subq)))
+print(render(query))
